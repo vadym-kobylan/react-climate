5f41d9a0845e66b312121635829957d87fe605d0
vadym-kobylan <vadym.kobylan@gmail.com>
2025-05-22 19:14:17 +0300

    feat-2: Search History added

--- src/components/CitySearch.tsx
+++ src/components/CitySearch.tsx
@@ -9,9 +9,11 @@ import {
   CommandList,
   CommandSeparator,
 } from './ui/command';
-import { Loader2, Search } from 'lucide-react';
+import { Clock, Loader2, Search, XCircle } from 'lucide-react';
 import { useLocationSearch } from '@/hooks/useWeather';
 import { useNavigate } from 'react-router-dom';
+import { useSearchHistory } from '@/hooks/useSearchHistory';
+import { format } from 'date-fns';
 
 const CitySearch = () => {
   const [open, setOpen] = useState(false);
@@ -19,11 +21,19 @@ const CitySearch = () => {
   const navigate = useNavigate();
 
   const { data: locations, isLoading } = useLocationSearch(query);
+  const { history, clearHistory, addToHistory } = useSearchHistory();
 
   const handleSelect = (cityData: string) => {
     const [lat, lon, name, country] = cityData.split('|');
 
-    // Add to search history
+    addToHistory.mutate({
+      query,
+      name,
+      lat: parseFloat(lat),
+      lon: parseFloat(lon),
+      country,
+    });
+
     setOpen(false);
     navigate(`/city/${name}?lat=${lat}&lon=${lon}`);
   };
@@ -42,15 +52,41 @@ const CitySearch = () => {
           {query.length > 2 && !isLoading && <CommandEmpty>No results found.</CommandEmpty>}
           {/* <CommandGroup heading="Favorites">
             <CommandItem>Calendar</CommandItem>
-          </CommandGroup>
+          </CommandGroup> */}
+
+          {history.length > 0 && (
+            <>
+              <CommandSeparator />
+              <CommandGroup>
+                <div className="flex items-center justify-between px-2 my-2">
+                  <p className="text-xs text-muted-foreground">Recent Searches</p>
+                  <Button variant={'ghost'} size={'sm'} onClick={() => clearHistory.mutate()}>
+                    <XCircle className="h-4 w-4" />
+                    Clear
+                  </Button>
+                </div>
+
+                {history.map((location) => (
+                  <CommandItem
+                    key={`${location.lat}-${location.lon}`}
+                    value={`${location.lat}|${location.lon}|${location.name}|${location.country}`}
+                    onSelect={handleSelect}>
+                    <Clock className="h-4 w-4 mr-2" />
+                    <span>{location.name}</span>
+                    {location.state && (
+                      <span className="text-sm text-muted-foreground">, {location.state}</span>
+                    )}
+                    <span className="text-sm text-muted-foreground">, {location.country}</span>
+                    <span className="ml-auto text-sm text-muted-foreground">
+                      {format(location.searchedAt, 'MMM d, h:mm a')}
+                    </span>
+                  </CommandItem>
+                ))}
+              </CommandGroup>
+            </>
+          )}
 
           <CommandSeparator />
-
-          <CommandGroup heading="Recent Search">
-            <CommandItem>Calendar</CommandItem>
-          </CommandGroup>
-
-          <CommandSeparator /> */}
 
           {locations && locations.length > 0 && (
             <CommandGroup heading="Suggestions">
--- src/pages/WeatherDashboard.tsx
+++ src/pages/WeatherDashboard.tsx
@@ -106,7 +106,7 @@ const WeatherDashboard = () => {
           <CurrentWeather data={weatherQuery.data} locationName={locationName} />
           <HourlyTemperature data={forecastQuery.data} />
         </div>
-        <div className="grid gap-6 md:grid-cols-2 items-start">
+        <div className="grid gap-6 lg:grid-cols-2 items-start">
           <WeatherDetails data={weatherQuery.data} />
           <WeatherForecast data={forecastQuery.data} />
         </div>
