db8624bba3a6b72dbfededc1dbfd981045e1f277
vadym-kobylan <vadym.kobylan@gmail.com>
2025-05-23 20:22:12 +0300

    fix: build fix

--- src/pages/CityPage.tsx
+++ src/pages/CityPage.tsx
@@ -14,8 +14,8 @@ const CityPage = () => {
   const [searchParams] = useSearchParams();
   const params = useParams();
 
-  const lat = parseFloat(searchParams.get('lat' || '0'));
-  const lon = parseFloat(searchParams.get('lon' || '0'));
+  const lat = parseFloat(searchParams.get('lat') || '0');
+  const lon = parseFloat(searchParams.get('lon') || '0');
 
   const coordinates = {
     lat,
